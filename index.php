02275d893c4b2ba7a5fd470679d7aeaed9a7f91a
Tr4xb0y69 <65660796+Tr4xb0y69@users.noreply.github.com>
2020-07-25 20:38:46 -0300

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>Tr4xb0y</title>
+    <title>$</title>
     <link rel="stylesheet" href="styles/index.css" />
     <script
       src="https://kit.fontawesome.com/7099ec12ad.js"
